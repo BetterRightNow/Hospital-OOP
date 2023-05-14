e1642f54849eed2b43dd4594e235c8766aa4af96
Pavel Dzemiashkevich <demeshkevich.pavel@gmail.com>
2023-05-14 14:06:05 +0200

    custom CheckDentistry<> in Dentistry class

--- src/main/java/com/solvd/laba/Classes/Dentistry.java
+++ src/main/java/com/solvd/laba/Classes/Dentistry.java
@@ -6,16 +6,32 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.function.Consumer;
 
+interface CheckDentistry<T> {
+    void apply(T t);
+}
+
+
+
 public class Dentistry extends Hospital {
     private int doctorsFee;
+    private int maxPatientsInQueue;
 
     private Queue<Patients> dentistryQueue;
 
 
-    public Dentistry(String name, Address address, int doctorsFee) throws InvalidCountryException {
+    public Dentistry(String name, Address address, int doctorsFee, int maxPatientsInQueue) throws InvalidCountryException {
         super(name, address);
         this.doctorsFee = doctorsFee;
         dentistryQueue = new LinkedList<>();
+        this.maxPatientsInQueue = maxPatientsInQueue;
+    }
+
+    public int getMaxPatientsInQueue() {
+        return maxPatientsInQueue;
+    }
+
+    public void setMaxPatientsInQueue(int maxPatientsInQueue) {
+        this.maxPatientsInQueue = maxPatientsInQueue;
     }
 
     public int getDoctorsFee() {
@@ -43,6 +59,18 @@ public class Dentistry extends Hospital {
             }
         };
         krakowFilter.accept(dentistryQueue);
+    }
+
+    public void checkQueue () {
+        CheckDentistry<Dentistry> checkDent = (Dentistry dentistry) -> {
+            int freePlaces = maxPatientsInQueue - dentistryQueue.size();
+            if (dentistryQueue.size() > maxPatientsInQueue) {
+                System.out.println("The queue at dentistry " + dentistry.name + " is overcrowded");
+            } else {
+                System.out.println("The queue at dentistry " + dentistry.name + " is ok, number of free places is " + freePlaces);
+            }
+        };
+        checkDent.apply(this);
     }
 
     @Override
--- src/main/java/com/solvd/laba/Classes/Main.java
+++ src/main/java/com/solvd/laba/Classes/Main.java
@@ -27,6 +27,7 @@ public class Main {
 //    Consumer<Queue<Patients>> in Dentistry class
 //    Function <> in PrivateClinic class
 //    custom Ambulance<> in EmergencyHospital class
+//    custom CheckDentistry<> in Dentistry class
 
 
 
@@ -73,7 +74,7 @@ public class Main {
         System.out.println(nurse1.calculateYearlySalary());
 
 //        working with Dentistry class
-        Dentistry dent1 = new Dentistry("Warsaw dentistry", address1, 300);
+        Dentistry dent1 = new Dentistry("Warsaw dentistry", address1, 300, 10);
         System.out.println(dent1);
 
 
@@ -174,5 +175,9 @@ public class Main {
 //        working with Ambulance<> in EmergencyHospital class
         System.out.println("\nworking with Ambulance<> in EmergencyHospital class");
         System.out.println(emergencyHospital1.ambulanceTeam());
+
+//        working with CheckDentistry<> in Dentistry class
+        System.out.println("\nworking with CheckDentistry<> in Dentistry class");
+        dent1.checkQueue();
     }
 }
